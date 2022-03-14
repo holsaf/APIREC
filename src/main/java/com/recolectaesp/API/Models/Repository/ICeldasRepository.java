4366632d6a10e82314dd84194e825ed1b89d9da7
holsaf <hol.tk@hotmail.com>
2022-03-13 19:42:32 -0500

    API configurada con base de datos en AWS

--- src/main/java/com/recolectaesp/API/Models/Repository/ICeldasRepository.java
+++ src/main/java/com/recolectaesp/API/Models/Repository/ICeldasRepository.java
@@ -13,7 +13,7 @@ public interface ICeldasRepository extends JpaRepository<Celdas , Long> {
 
     // consulta para saber la mejor celda disponible, dependiendo del tipo de residuo y deposito se escoge la de
     // mayor capacidad disponible.
-    @Query(value = "SELECT * FROM REC_CELDAS c WHERE c.id_tipo_residuo = :tipoResiduo AND c.id_deposito = :deposito " +
+    @Query(value = "SELECT * FROM rec_celdas c WHERE c.id_tipo_residuo = :tipoResiduo AND c.id_deposito = :deposito " +
             "ORDER BY c.capacidad_disponible DESC LIMIT 1", nativeQuery = true)
     Celdas findCeldaDisponibleByTipoResiduoAndDeposito(@Param("tipoResiduo") Integer id_tipo_residuo, @Param("deposito") Integer id_deposito);
 
